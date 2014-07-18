16c1b3347f8c243b1f9ff5c5fd7bc9fc1cf1769d
marco <marco@marco-laptop>
2014-07-18 10:39:14 +0200

    setup ware

--- Programmieren_Aufgaben/Probeklausur/com/probeklausur/Ware.java
+++ Programmieren_Aufgaben/Probeklausur/com/probeklausur/Ware.java
@@ -2,4 +2,12 @@ package com.probeklausur;
 
 public class Ware {
 
+	private String titel;
+	private String beschreibung;
+	
+	public Ware (String titel, String beschreibung) {
+		this.titel = titel;
+		this.beschreibung = beschreibung;
+	}
+	
 }
