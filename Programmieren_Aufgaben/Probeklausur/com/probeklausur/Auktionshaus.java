1aba758077d287109368593f0be23bbf46083866
marco <marco@marco-laptop>
2014-07-18 11:03:55 +0200

    setup auktionshaus

--- Programmieren_Aufgaben/Probeklausur/com/probeklausur/Auktionshaus.java
+++ Programmieren_Aufgaben/Probeklausur/com/probeklausur/Auktionshaus.java
@@ -1,5 +1,22 @@
 package com.probeklausur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Auktionshaus {
+
+	private ArrayList<Auktion> alleAuktionen = new ArrayList<Auktion>();
+
+	public void addAuktion(Auktion a) {
+		alleAuktionen.add(a);
+	}
+
+	public void removeAuktion(Auktion a) {
+		alleAuktionen.remove(a);
+	}
+
+	public List<Auktion> getAuktionen() {
+		return alleAuktionen;
+	}
 
 }
