e4709d4faa18ca359e74d6b1b441bf9921bfb79f
marco <marco@marco-laptop>
2014-07-18 10:37:40 +0200

    setup bieter

--- Programmieren_Aufgaben/Probeklausur/com/probeklausur/Bieter.java
+++ Programmieren_Aufgaben/Probeklausur/com/probeklausur/Bieter.java
@@ -1,5 +1,18 @@
 package com.probeklausur;
 
+
 public class Bieter {
 
+	private String vorname;
+	private String nachname;
+	
+	public Bieter(String vor, String nach) {
+		vorname = vor;
+		nachname = nach;
+	}
+	
+	public String getFullName() {
+		return vorname + " " + nachname;
+	}
+	
 }
