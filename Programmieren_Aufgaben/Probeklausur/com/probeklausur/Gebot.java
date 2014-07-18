5af098c4d6d927ee659f37bc341b6ba19cf42dce
marco <marco@marco-laptop>
2014-07-18 10:41:47 +0200

    setup gebot

--- Programmieren_Aufgaben/Probeklausur/com/probeklausur/Gebot.java
+++ Programmieren_Aufgaben/Probeklausur/com/probeklausur/Gebot.java
@@ -2,4 +2,13 @@ package com.probeklausur;
 
 public class Gebot {
 
+	private double maxGebot;
+	private Bieter bieter;
+	
+	public Gebot(Bieter b, double maxGebot){
+		bieter = b;
+		this.maxGebot = maxGebot;
+	}
+	
+	
 }
